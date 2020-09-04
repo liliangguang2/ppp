a25b5e9c02342d01940672c94d7ab596ded46ba8
841685520@qq.com <858973780@qq.com>
2020-09-04 18:23:49 +0800

    first class :ppp

---
+++ src/cn/kgc/Helloppp.java
@@ -0,0 +1,13 @@
+package cn.kgc;
+
+public class Helloppp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("other ordertest");
+	}
+
+}
